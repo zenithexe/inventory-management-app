a4b4a09e460c51fafa708e5cc70e888874cd7e80
Jalish Buktawar <jalish.dev@gmail.com>
2024-07-21 13:03:07 +0530

    Fixed MongoDB URI

--- mongodb/connect.js
+++ mongodb/connect.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 export default async function connectMongo(){
     
-    mongoose.connect(process.env.DB_LINK)
+    mongoose.connect(process.env.MONGODB_URI)
     .then(()=> console.log("Connected to DB"))
     .catch((err) => console.error('Error :', err))
     
